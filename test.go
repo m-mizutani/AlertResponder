d5d2030eb61bbe82333da9423c498db3359281ac
Masayoshi Mizutani <mizutani@cookpad.com>
2018-09-26 09:53:47 +0900

    Add section titles as comment to each resource

--- test.go
+++ test.go
@@ -6,6 +6,7 @@ import (
 
 func doIntegrationTest(stackName, region string, verbose bool) {
 	log.Println("=== Start integration test ===")
+
 	log.Println("=== Exit integration test ===")
 	return
 }
