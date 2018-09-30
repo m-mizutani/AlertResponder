861e0d73e088497d6e031e3c2345b9ac1dcba087
Masayoshi Mizutani <mizutani@cookpad.com>
2018-09-30 12:59:20 +0900

    Add DynamoDB into emitter test template to save result of test

--- test/emitter/deploy.sh
+++ test/emitter/deploy.sh
@@ -18,48 +18,29 @@ echo "Output Template: $OUTPUT_FILE"
 echo ""
 
 ResponderResources=`aws cloudformation describe-stack-resources --stack-name $1 | jq '.StackResources[]'`
-
 TaskStream=`echo $ResponderResources | jq 'select(.LogicalResourceId == "TaskStream")'`
-
-TaskStreamName=`echo $TaskStream | jq .PhysicalResourceId -r`
 Region=`echo $TaskStream | jq .StackId -r | cut -d : -f 4`
 AccountID=`echo $TaskStream | jq .StackId -r | cut -d : -f 5`
-
-ReportDataName=`echo $ResponderResources | jq 'select(.LogicalResourceId == "ReportData") | .PhysicalResourceId' -r`
-
-TaskStreamArn="arn:aws:kinesis:$Region:$AccountID:stream/$TaskStreamName"
 ReportLineArn=`echo $ResponderResources | jq 'select(.LogicalResourceId == "ReportLine") | .PhysicalResourceId' -r`
-IncidentLineArn=`echo $ResponderResources | jq 'select(.LogicalResourceId == "IncidentLine") | .PhysicalResourceId' -r`
 
+aws cloudformation package --template-file $TEMPLATE_FILE \
+    --output-template-file $OUTPUT_FILE --s3-bucket $3 --s3-prefix $4
+aws cloudformation deploy --template-file $OUTPUT_FILE --stack-name $2 \
+    --capabilities CAPABILITY_IAM \
+    --parameter-overrides $OPT \
+    ReportLineArn=$ReportLineArn
 
-# aws cloudformation package --template-file $TEMPLATE_FILE \
-#     --output-template-file $OUTPUT_FILE --s3-bucket $3 --s3-prefix $4
-# aws cloudformation deploy --template-file $OUTPUT_FILE --stack-name $2 \
-#     --capabilities CAPABILITY_IAM \
-#     --parameter-overrides $OPT \
-#     ReportLineArn=$ReportLineArn \
-#     IncidentLineArn=$IncidentLineArn \
-#     TaskStreamArn=$TaskStreamArn \
-#     ReportDataName=$ReportDataName 
-# 
 Resources=`aws cloudformation describe-stack-resources --stack-name $2 | jq '.StackResources[]'`
+ReportResultsArn=`echo $Resources | jq 'select(.LogicalResourceId == "ReportResults") | .PhysicalResourceId' -r`
 
-ReportReceiverArn=`echo $Resources | jq 'select(.LogicalResourceId == "ReportReceiver") | .PhysicalResourceId' -r`
-IncidentReceiverArn=`echo $Resources | jq 'select(.LogicalResourceId == "IncidentReceiver") | .PhysicalResourceId' -r`
-
-echo $Resources | jq .
-echo $ReportReceiverArn
-echo $IncidentReceiverArn
 
 cat <<EOF > test.json
 {
   "StackName": "$1",
   "Region": "$Region",
-  "ReportReceiverArn": "$ReportReceiverArn",
-  "IncidentReceiverArn": "$IncidentReceiverArn"
+  "ReportResults": "$ReportResultsArn"
 }
 EOF
 
 echo ""
 echo "done"
-
---
+++ test/emitter/main.py
@@ -0,0 +1,37 @@
+import json
+import boto3
+import logging
+import os
+import datetime
+
+
+logger = logging.getLogger()
+logger.setLevel(level=logging.INFO)
+
+
+def iter_sns_msg(records):
+    for record in records.get('Records', []):
+        try:
+            sns_msg = record.get('Sns', {}).get('Message')
+            yield json.loads(sns_msg)
+        except Exception as e:
+            logger.error(e)
+
+
+def handler(records, context):
+    client = boto3.client('dynamodb')
+
+    for msg in iter_sns_msg(records):
+        logger.info(json.dumps(msg, indent=2))
+
+        res = client.put_item(
+            TableName=os.environ["TABLE_NAME"],
+            Item={
+                'report_id': {'S': msg['report_id']},
+                'timestamp': {'N': str(datetime.datetime.utcnow().timestamp())},
+                'report': {'S': json.dumps(msg)},
+            })
+        
+        logger.info(res)
+         
+    return {'message': 'ok'}
