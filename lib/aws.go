9dacf227b7e325f6f740b9b22895c31d4ace21fa
Masayoshi Mizutani <mizutani@cookpad.com>
2018-10-03 11:06:08 +0900

    Add GetSecretValues to use SecretsManager

--- lib/aws.go
+++ lib/aws.go
@@ -3,12 +3,14 @@ package lib
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"strings"
 	"time"
 
 	"github.com/aws/aws-lambda-go/lambdacontext"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/secretsmanager"
 	"github.com/aws/aws-sdk-go/service/sfn"
 	"github.com/aws/aws-sdk-go/service/sns"
 
@@ -100,6 +102,35 @@ func PublishSnsMessage(topicArn, region string, report *Report) error {
 
 	if err != nil {
 		return errors.Wrap(err, "Fail to publish report")
+	}
+
+	return nil
+}
+
+func GetSecretValues(secretArn string, values interface{}) error {
+	// sample: arn:aws:secretsmanager:ap-northeast-1:1234567890:secret:mytest
+	arn := strings.Split(secretArn, ":")
+	if len(arn) != 7 {
+		return errors.New(fmt.Sprintf("Invalid SecretsManager ARN format: %s", secretArn))
+	}
+	region := arn[3]
+
+	ssn := session.Must(session.NewSession(&aws.Config{
+		Region: aws.String(region),
+	}))
+	mgr := secretsmanager.New(ssn)
+
+	result, err := mgr.GetSecretValue(&secretsmanager.GetSecretValueInput{
+		SecretId: aws.String(secretArn),
+	})
+
+	if err != nil {
+		return errors.Wrap(err, "Fail to retrieve secret values")
+	}
+
+	err = json.Unmarshal([]byte(*result.SecretString), values)
+	if err != nil {
+		return errors.Wrap(err, "Fail to parse secret values as JSON")
 	}
 
 	return nil
