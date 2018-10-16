a3a15f30d8ce15bebae6455de3d87e91950a6bd8
Masayoshi Mizutani <mizutani@cookpad.com>
2018-10-16 10:16:09 +0900

    Use logrus for logging

--- functions/compiler/compiler.go
+++ functions/compiler/compiler.go
@@ -7,6 +7,7 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/m-mizutani/AlertResponder/lib"
 	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
 )
 
 type CompiledReport struct {
@@ -34,17 +35,19 @@ func buildParameters(ctx context.Context) (*parameters, error) {
 
 // HandleRequest is a main Lambda handler
 func HandleRequest(ctx context.Context, report lib.Report) (*lib.Report, error) {
+	log.WithField("report", report).Info("start")
+
 	params, err := buildParameters(ctx)
 	if err != nil {
 		return nil, err
 	}
 
-	lib.Dump("report", report)
-
 	pages, err := lib.FetchReportPages(params.tableName, params.region, report.ID)
 	if err != nil {
 		return nil, err
 	}
+
+	log.WithField("pages", pages).Info("Fetched pages")
 
 	c := &report.Content
 	c.RemoteHosts = map[string]lib.ReportRemoteHost{}
@@ -52,12 +55,14 @@ func HandleRequest(ctx context.Context, report lib.Report) (*lib.Report, error) 
 
 	for _, page := range pages {
 		for _, r := range page.RemoteHost {
+			log.WithField("id", r.ID).Info("set section to remote")
 			h, _ := c.RemoteHosts[r.ID]
 			h.Merge(r)
 			c.RemoteHosts[r.ID] = h
 		}
 
 		for _, r := range page.LocalHost {
+			log.WithField("id", r.ID).Info("set section to local")
 			h, _ := c.LocalHosts[r.ID]
 			h.Merge(r)
 			c.LocalHosts[r.ID] = h
@@ -68,5 +73,19 @@ func HandleRequest(ctx context.Context, report lib.Report) (*lib.Report, error) 
 }
 
 func main() {
+	log.SetFormatter(&log.JSONFormatter{})
+	switch os.Getenv("LOG_LEVEL") {
+	case "error":
+		log.SetLevel(log.ErrorLevel)
+	case "warn":
+		log.SetLevel(log.WarnLevel)
+	case "info":
+		log.SetLevel(log.InfoLevel)
+	case "debug":
+		log.SetLevel(log.DebugLevel)
+	default:
+		log.SetLevel(log.InfoLevel)
+	}
+
 	lambda.Start(HandleRequest)
 }
